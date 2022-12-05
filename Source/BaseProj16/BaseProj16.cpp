54edac5cbc41b27a54d5a805027145de0d437eef
Olsta7 <olliemoss95@aol.com>
2022-12-05 17:47:31 +0000

    Base Project

---
+++ Source/BaseProj16/BaseProj16.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BaseProj16.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BaseProj16, "BaseProj16" );
+ 
