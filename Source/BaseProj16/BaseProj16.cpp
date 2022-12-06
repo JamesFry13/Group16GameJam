8602c8152da920f6218b599b26123c752d8ba9ec
Olsta7 <olliemoss95@aol.com>
2022-12-06 16:19:08 +0000

    Overhauled project to Blueprint
    Lots of issues with C++ classes and Rider integration

--- Source/BaseProj16/BaseProj16.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "BaseProj16.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BaseProj16, "BaseProj16" );
- 
